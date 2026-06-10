6a9c01ccb8c2b3cd34c14c4b868456d1676837a2
Ankit0830 <ankitkumarsahu01299@gmail.com>
2026-06-10 22:01:03 +0530

    add MySql database

--- student_result_manager.py
+++ student_result_manager.py
@@ -1,118 +1,298 @@
-student = {}
+# import cursor from database
+from db_connection import conn, cursor
 
 while True:
     print("\n----- STUDENT MANAGER APP -----\n")
-    print("1. Add Student")
-    print("2. Delete Student")
-    print("3. View Student")
-    print("4. Student Grade")
-    print("5. Update Marks")
-    print("6. Student Percentage")
-    print("7. Check Result")
-    print("8. Show Topper")
-    print("9. Exit")
+    print("1. Add Students")
+    print("2. Add Subject Marks")
+    print("3. Delete Students")
+    print("4. View Students")
+    print("5. Students Grade")
+    print("6. Update Marks")
+    print("7. Students Percentage")
+    print("8. Check Result")
+    print("9. Show Topper")
+    print("10. Exit")
 
     # Add student
     choice = input("Enter your choice: ")
 
     if choice == "1":
         name = input("Enter student name: ")
-        marks = int(input("Enter marks: "))
-        student[name] = marks
-        print(f"{name} Successfully Added!")
+        
+        query = """
+        INSERT INTO students(name)
+        VALUES(%s)
+        """
+
+        cursor.execute(query, (name,))
+        conn.commit()
+
+        print("Student Added Successfully!")
+
+    # add subject and marks
+    elif choice == "2":
+        student_id = int(input("Enter Student ID: "))
+        subject = input("Enter Subject Name: ")
+        marks = int(input("Enter Marks: "))
+
+        query = """
+        INSERT INTO marks
+        (student_id, subject_name, marks)
+        VALUES(%s, %s, %s)
+        """
+
+        cursor.execute(
+            query,
+            (student_id, subject, marks)
+        )
+
+        conn.commit()
+
+        print("Subject Marks Added Successfully!")
 
     # Delete student 
-    elif choice == "2":
-        name = input("Enter name of student:")
-        if name in student:
-            student.pop(name)
-            print(f"{name} Successfully deleted!")
+    elif choice == "3":
+        student_id = int(input("Enter Student ID: "))
+
+        query = """
+        DELETE FROM students
+        WHERE id = %s
+        """
+
+        cursor.execute(query, (student_id,))
+        conn.commit()
+
+        if cursor.rowcount > 0:
+            print("Student Deleted Successfully!")
         else:
-            print("Student not found!")
+            print("Student ID not found!")
+        # if name in students:
+        #     students.pop(name)
+        #     print(f"{name} Successfully deleted!")
+        # else:
+        #     print("Student not found!")
 
     # View student
-    elif choice == "3":
-        if not student:
-            print("No student found!")
+    elif choice == "4":
+        query = """
+        SELECT s.id,
+            s.name,
+            m.subject_name,
+            m.marks
+        FROM students s
+        JOIN marks m
+        ON s.id = m.student_id
+        ORDER BY s.id
+        """
 
+        cursor.execute(query)
+        records = cursor.fetchall()
+
+        if records:
+            print("\nID\tName\tMarks")
+            print("-" * 25)
+
+            for row in records:
+                print(f"{row[0]}\t{row[1]}\t{row[2]}")
         else:
-            for name, marks in student.items():
-                print(name, ":", marks)
+            print("No students found!")
+
+        # if not student:
+        #     print("No student found!")
+
+        # else:
+        #     for name, marks in student.items():
+        #         print(name, ":", marks)
 
     # Grade of student
-    elif choice == "4":
-        name = input("Enter student name: ")
+    elif choice == "5":
+        student_id = int(input("Enter Student ID: "))
 
-        if name in student:
-            marks = student[name]
+        # query = "SELECT marks FROM students WHERE id = %s"
+        query = """
+        SELECT AVG(marks)
+        FROM marks
+        WHERE student_id = %s"""
 
-            if marks >= 95:
+        cursor.execute(query, (student_id,))
+        result = cursor.fetchone()
+
+        if result:
+            percentage = result[0]
+
+            if percentage >= 95:
                 print("A+ Grade")
-
-            elif marks <= 95 and marks >= 75:
+            elif percentage >= 75:
                 print("A Grade")
-
-            elif marks <= 75 and marks >= 50:
+            elif percentage >= 50:
                 print("B Grade")
-
-            elif marks <= 50 and marks >= 25:
+            elif percentage >= 25:
                 print("C Grade")
-
             else:
-                print("FAIL!")
-
+                print("FAIL")
         else:
             print("Student not found!")
+
+    # elif choice == "4":
+    #     name = input("Enter student name: ")
+
+    #     if name in students:
+    #         marks = students[name]
+
+    #         if marks >= 95:
+    #             print("A+ Grade")
+
+    #         elif marks <= 95 and marks >= 75:
+    #             print("A Grade")
+
+    #         elif marks <= 75 and marks >= 50:
+    #             print("B Grade")
+
+    #         elif marks <= 50 and marks >= 25:
+    #             print("C Grade")
+
+    #         else:
+    #             print("FAIL!")
+
+    #     else:
+    #         print("Students not found!")
 
     # Update marks
-    elif choice == "5":
-        name = input("Enter student name:")
-
-        if name in student:
-            new_marks = int(input("Enter new marks: "))
-            student[name] = new_marks
-            # student.update([name: new_marks])
-            print("Marks updated successfully!")
-        else:
-            print("Student not found!")
-
-    # Student percentage
     elif choice == "6":
-        name = input("Enter student name: ")
+        student_id = int(input("Enter Student ID: "))
+        new_marks = int(input("Enter New Marks: "))
 
-        if name in student:
-            marks = student[name]
-            percentage = (marks / 100) * 100
-            print(f"Percentage: {percentage}%")
+        if 0 <= new_marks <= 100:
+
+            query = """
+            UPDATE students
+            SET marks = %s
+            WHERE id = %s
+            """
+
+            cursor.execute(query, (new_marks, student_id))
+            conn.commit()
+
+            if cursor.rowcount > 0:
+                print("Marks Updated Successfully!")
+            else:
+                print("Student ID not found!")
+
         else:
-            print("Student not found!")
+            print("Marks must be between 0 and 100")
+
+            # if name in students:
+            #     new_marks = int(input("Enter new marks: "))
+            #     students[name] = new_marks
+            #     # student.update([name: new_marks])
+            #     print("Marks updated successfully!")
+            # else:
+            #     print("Students not found!")
+
+    # Students percentage
+    elif choice == "7":
+        student_id = int(input("Enter Student ID: "))
+
+        query = """
+        SELECT AVG(marks)
+        FROM marks
+        WHERE student_id = %s
+        """
+
+        cursor.execute(query, (student_id,))
+
+        result = cursor.fetchone()
+
+        if result and result[0]:
+            print(f"Percentage: {result[0]:.2f}%")
+        else:
+            print("No marks found!")
+
+
+    # elif choice == "6":
+    #     name = input("Enter student name: ")
+
+    #     if name in students:
+    #         marks = students[name]
+    #         percentage = (marks / 100) * 100
+    #         print(f"Percentage: {percentage}%")
+    #     else:
+    #         print("Students not found!")
 
 
     # Check result
-    elif choice == "7":
-        name = input("Enter student name:")
+    elif choice == "8":
+        student_id = int(input("Enter Student ID: "))
 
-        if name in student:
-            marks = student[name]
+        query = """
+        SELECT AVG(marks)
+        FROM marks
+        WHERE student_id = %s
+        """
 
-            if marks >= 40:
-                print("PASS! ")
+        cursor.execute(query, (student_id,))
+        result = cursor.fetchone()
+
+        if result:
+            average = result[0]
+
+            if average >= 40:
+                print("PASS!")
             else:
                 print("FAIL!")
         else:
-            print("Student Not Found! ")
+            print("Student not found!")
+
+
+
+        # name = input("Enter students name:")
+
+        # if name in students:
+        #     marks = students[name]
+
+        #     if marks >= 40:
+        #         print("PASS! ")
+        #     else:
+        #         print("FAIL!")
+        # else:
+        #     print("Students Not Found! ")
 
     # Show Topper
-    elif choice == "8":
-        if student:
-            topper = max(student, key=student.get)
-            print(f"Topper:{topper} \n Marks: {student[topper]}")
+    elif choice == "9":
+        query = """
+        SELECT s.id,
+            s.name,
+            AVG(m.marks) AS percentage
+        FROM students s
+        JOIN marks m
+        ON s.id = m.student_id
+        GROUP BY s.id, s.name
+        ORDER BY percentage DESC
+        LIMIT 1
+        """
 
+        cursor.execute(query)
+        topper = cursor.fetchone()
+
+        if topper:
+            print("\nTopper Details")
+            print(f"ID: {topper[0]}")
+            print(f"Name: {topper[1]}")
+            print(f"Marks: {topper[2]}")
         else:
-            print("No student found!")
+            print("No students found!")
+
+    # elif choice == "8":
+    #     if students:
+    #         topper = max(students, key=student.get)
+    #         print(f"Topper:{topper} \n Marks: {students[topper]}")
+
+    #     else:
+    #         print("No student found!")
     
     # Exit
-    elif choice == "9":
+    elif choice == "10":
         print("Exiting.........")
         break
 
